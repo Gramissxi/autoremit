94614b6d5bf9c0323eea193b1a56a5e871fcfdf6
Gramissxi <gramissxi@gmail.com>
2026-06-02 21:22:10 -0300

    preparacion para el despliegue

--- autoremit/settings.py
+++ autoremit/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-@@r8#4%mx+lzyfc02+6*vxll#1#37_0+$26p3&+jl2hkno6y#q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Application definition
